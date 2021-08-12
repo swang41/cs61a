97a0045c69a7de3bc4c866a64ff0d50913f5800e
Wang, Sikai <sikai.wang@blueshieldca.com>
2021-08-12 15:54:11 -0700

    first commit

---
+++ hw/hw01/hw01.py
@@ -0,0 +1,234 @@
+from operator import add, mul, sub
+
+square = lambda x: x * x
+
+identity = lambda x: x
+
+triple = lambda x: 3 * x
+
+increment = lambda x: x + 1
+
+
+HW_SOURCE_FILE = __file__
+
+
+from operator import add, sub
+
+
+
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    """
+    if b < 0:
+        f = lambda a, b: add(a, -b)
+    else:
+        f = lambda a, b: add(a, b)
+    return f(a, b)
+
+def two_of_three(a, b, c):
+    """Return x*x + y*y, where x and y are the two largest members of the
+    positive numbers a, b, and c.
+
+    >>> two_of_three(1, 2, 3)
+    13
+    >>> two_of_three(5, 3, 1)
+    34
+    >>> two_of_three(10, 2, 8)
+    164
+    >>> two_of_three(5, 5, 5)
+    50
+    """
+    return max(a*a+b*b, a*a+c*c, b*b+c*c)
+
+def largest_factor(n):
+    """Return the largest factor of n that is smaller than n.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factor is 1 since 13 is prime
+    1
+    """
+    res = 1
+    i = 2
+    while i*i <= n:
+        if n % i == 0:
+            res = max(res, i, n//i)
+        i += 1
+    return res
+
+
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    "*** YOUR CODE HERE ***"
+    print(n)
+    if n == 1:
+        return 1
+    elif n % 2 == 0:
+        return 1+hailstone(n//2)
+    else:
+        return 1+hailstone(n*3+1)
+
+def product(n, term):
+    """Return the product of the first n terms in a sequence.
+    n -- a positive integer
+    term -- a function that takes one argument to produce the term
+
+    >>> product(3, identity)  # 1 * 2 * 3
+    6
+    >>> product(5, identity)  # 1 * 2 * 3 * 4 * 5
+    120
+    >>> product(3, square)    # 1^2 * 2^2 * 3^2
+    36
+    >>> product(5, square)    # 1^2 * 2^2 * 3^2 * 4^2 * 5^2
+    14400
+    >>> product(3, increment) # (1+1) * (2+1) * (3+1)
+    24
+    >>> product(3, triple)    # 1*3 * 2*3 * 3*3
+    162
+    """
+    "*** YOUR CODE HERE ***"
+    res = 1
+    for i in range(1, n+1):
+        res *= term(i)
+    return res
+
+def square(x):
+    return x * x
+
+
+def accumulate(merger, base, n, term):
+    """Return the result of merging the first n terms in a sequence and base.
+    The terms to be merged are term(1), term(2), ..., term(n). merger is a
+    two-argument commutative function.
+
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)    # 2 * 1^2 * 2^2 * 3^2
+    72
+    >>> accumulate(lambda x, y: x + y + 1, 2, 3, square)
+    19
+    >>> accumulate(lambda x, y: 2 * (x + y), 2, 3, square)
+    58
+    >>> accumulate(lambda x, y: (x + y) % 17, 19, 20, square)
+    16
+    """
+    "*** YOUR CODE HERE ***"
+    res = base
+    for i in range(1, n+1):
+        res = merger(res, term(i))
+    return res
+
+def summation_using_accumulate(n, term):
+    """Returns the sum of term(1) + ... + term(n). The implementation
+    uses accumulate.
+
+    >>> summation_using_accumulate(5, square)
+    55
+    >>> summation_using_accumulate(5, triple)
+    45
+    >>> from construct_check import check
+    >>> # ban iteration and recursion
+    >>> check(HW_SOURCE_FILE, 'summation_using_accumulate',
+    ...       ['Recursion', 'For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    return accumulate(add, 0, n, term)
+
+def product_using_accumulate(n, term):
+    """An implementation of product using accumulate.
+
+    >>> product_using_accumulate(4, square)
+    576
+    >>> product_using_accumulate(6, triple)
+    524880
+    >>> from construct_check import check
+    >>> # ban iteration and recursion
+    >>> check(HW_SOURCE_FILE, 'product_using_accumulate',
+    ...       ['Recursion', 'For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    return accumulate(mul, 1, n, term)
+
+def if_function(condition, true_result, false_result):
+    """Return true_result if condition is a true value, and
+    false_result otherwise.
+
+    >>> if_function(True, 2, 3)
+    2
+    >>> if_function(False, 2, 3)
+    3
+    >>> if_function(3==2, 'equal', 'not equal')
+    'not equal'
+    >>> if_function(3>2, 'bigger', 'smaller')
+    'bigger'
+    """
+    if condition:
+        return true_result
+    else:
+        return false_result
+
+
+def with_if_statement():
+    """
+    >>> result = with_if_statement()
+    61A
+    >>> print(result)
+    None
+    """
+    if cond():
+        return true_func()
+    else:
+        return false_func()
+
+
+def with_if_function():
+    """
+    >>> result = with_if_function()
+    Welcome to
+    61A
+    >>> print(result)
+    None
+    """
+    return if_function(cond(), true_func(), false_func())
+
+
+def cond():
+    "*** YOUR CODE HERE ***"
+    return False
+
+
+def true_func():
+    "*** YOUR CODE HERE ***"
+    print("Welcome to")
+
+def false_func():
+    "*** YOUR CODE HERE ***"
+    print("61A")
